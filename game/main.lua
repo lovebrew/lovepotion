7e563d2ff5bdaf441ee3234e31e99c5a4e860932
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-09-19 16:45:37 +0100

    Make the sample quit-able

--- game/main.lua
+++ game/main.lua
@@ -3,3 +3,9 @@ function love.draw()
 	love.graphics.print("Replace me in game/main.lua.", 0, 0)
 
 end
+
+function love.keypressed(key, isrepeat)
+
+	if key == "start" then love.event.quit() end
+
+end
