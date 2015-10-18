26ac931e11429646667ab65fab69724b56df9934
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-10-18 22:47:47 +0100

    Remove openURL

--- source/modules/system.c
+++ source/modules/system.c
@@ -22,27 +22,6 @@
 
 #include "../shared.h"
 
-u8 buf0[0x300];
-u8 buf1[0x20]; // APT_DoAppJump() Buffers.
-
-static int systemOpenURL() { // love.system.openURL() -- Might not work on all systems.
-
-	// Clear buffers
-	memset(buf0, 0, 0x300);
-	memset(buf1, 0, 0x20);
-
-	aptOpenSession();
-
-	APT_PrepareToDoAppJump(NULL, 0, 0x20009D020, 0); // Browser App ID
-
-	APT_DoAppJump(NULL, 0x300, 0x20, buf0, buf1);
-
-	aptCloseSession();
-
-	return 0;
-
-}
-
 static int systemGetOS(lua_State *L) { // love.system.getOS()
 
 	lua_pushstring(L, "3ds");
@@ -82,7 +61,6 @@ static int systemGetPowerInfo(lua_State *L) { // love.system.getPowerInfo() TODO
 int initLoveSystem(lua_State *L) {
 
 	luaL_Reg reg[] = {
-		{ "openURL",		systemOpenURL		},
 		{ "getOS",			systemGetOS			},
 		{ "getPowerInfo",	systemGetPowerInfo	},
 		{ 0, 0 },
