42d130180c8ac846fd77d8922a345e37c5a006dd
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-11-20 23:05:50 +0000

    Sorry for all these commits :v

--- .travis-deps.sh
+++ .travis-deps.sh
@@ -12,7 +12,7 @@ cd ctrulib/libctru && make ; cd -
 cp -rf ctrulib/libctru/ ${DEVKITPRO}
 
 # Get picasso
-mv VideahGams/LovePotion/tools/picasso ${PICASSO}
+mv tools/picasso ${PICASSO}
 
 # Build and install portlibs
 mkdir ${DEVKITPRO}/portlibs && mkdir ${PORTLIBS}
