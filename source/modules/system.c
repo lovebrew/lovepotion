decf924a0b4db4d2fff5af9527af9d472001faa0
Substance12 <botproductions12@gmail.com>
2015-12-23 17:36:04 -0300

    system.c changes
    Edited love.system.getPowerInfo (seems to work fine now), and added
    love.system.getModel, love.system.getRegion and love.system.getLanguage.

--- source/modules/system.c
+++ source/modules/system.c
@@ -29,7 +29,136 @@ static int systemGetOS(lua_State *L) { // love.system.getOS()
 
 }
 
-static int systemGetPowerInfo(lua_State *L) { // love.system.getPowerInfo() TODO: Test this.
+static int systemGetModel(lua_State *L) { // love.system.getModel()
+	
+	u8 model;
+	CFGU_GetSystemModel(&model);
+	
+	if (model == 0) { // Original 3DS
+		
+		lua_pushstring(L, "o3ds");
+		
+	} else if (model == 1) { // 3DS XL
+		
+		lua_pushstring(L, "o3dsxl");
+		
+	} else if (model == 2) { // New 3DS
+		
+		lua_pushstring(L, "n3ds");
+		
+	} else if (model == 3) { // 2DS
+		
+		lua_pushstring(L, "2ds");
+		
+	} else if (model == 4) { // New 3DS XL
+		
+		lua_pushstring(L, "n3dsxl");
+		
+	}
+	
+	return 1;
+
+}
+
+static int systemGetLanguage(lua_State *L) { // love.system.getLanguage()
+	
+	u8 language;
+	CFGU_GetSystemLanguage(&language);
+	
+	if (language == CFG_LANGUAGE_JP) {  // Japanese
+		
+		lua_pushstring(L, "japanese");
+		
+	} else if (language == CFG_LANGUAGE_EN) { // English
+		
+		lua_pushstring(L, "english");
+		
+	} else if (language == CFG_LANGUAGE_FR) { // French
+		
+		lua_pushstring(L, "french");
+		
+	} else if (language == CFG_LANGUAGE_DE) { // German
+		
+		lua_pushstring(L, "german");
+		
+	} else if (language == CFG_LANGUAGE_IT) { // Italian
+		
+		lua_pushstring(L, "italian");
+		
+	} else if (language == CFG_LANGUAGE_ES) { // Spanish
+		
+		lua_pushstring(L, "spanish");
+		
+	} else if (language == CFG_LANGUAGE_ZH) { // Simplified Chinese
+		
+		lua_pushstring(L, "simplifiedchinese");
+		
+	} else if (language == CFG_LANGUAGE_KO) { // Korean
+		
+		lua_pushstring(L, "korean");
+		
+	} else if (language == CFG_LANGUAGE_NL) { // Dutch
+		
+		lua_pushstring(L, "dutch");
+		
+	} else if (language == CFG_LANGUAGE_PT) { // Portugese
+		
+		lua_pushstring(L, "portugese");
+		
+	} else if (language == CFG_LANGUAGE_RU) { // Russian
+		
+		lua_pushstring(L, "russian");
+		
+	} else if (language == CFG_LANGUAGE_TW) { // Traditional Chinese
+		
+		lua_pushstring(L, "traiditionalchinese");
+		
+	}
+	
+	return 1;
+
+}
+
+static int systemGetRegion(lua_State *L) { // love.system.getRegion()
+	
+	u8 region;
+	CFGU_SecureInfoGetRegion(&region);
+	
+	if (region == CFG_REGION_JPN) { // Japan
+		
+		lua_pushstring(L, "japan");
+		
+	} else if (region == CFG_REGION_USA) { // USA
+		
+		lua_pushstring(L, "usa");
+		
+	} else if (region == CFG_REGION_EUR) { // Europe
+		
+		lua_pushstring(L, "europe");
+		
+	} else if (region == CFG_REGION_AUS) { // Australia
+		
+		lua_pushstring(L, "australia");
+		
+	} else if (region == CFG_REGION_CHN) { // China
+		
+		lua_pushstring(L, "china");
+		
+	} else if (region == CFG_REGION_KOR) { // Korea
+		
+		lua_pushstring(L, "korea");
+		
+	} else if (region == CFG_REGION_TWN) { // Taiwan
+		
+		lua_pushstring(L, "taiwan");
+		
+	}
+	
+	return 1;
+
+}
+
+/* static int systemGetPowerInfo(lua_State *L) {
 
 	u8 batteryStateBool;
 	PTMU_GetBatteryChargeState(&batteryStateBool);
@@ -56,13 +185,42 @@ static int systemGetPowerInfo(lua_State *L) { // love.system.getPowerInfo() TODO
 
 	return 3;
 
+} */
+
+static int systemGetPowerInfo(lua_State *L){
+	
+	u8 batteryPercent;
+	
+	PTMU_GetBatteryLevel(&batteryPercent);
+	
+	u8 batteryStateBool;
+	PTMU_GetBatteryChargeState(&batteryStateBool);
+	
+	if (batteryStateBool == 0) {
+		
+		lua_pushstring(L, "battery");
+		
+	} else if (batteryStateBool == 1) {
+		
+		lua_pushstring(L, "charging");
+	
+	}
+	
+	lua_pushnumber(L, batteryPercent * 20); //PTMU_GetBatteryLevel returns a number between 0 and 5, so I multiply it for 20 to match LÖVE, which gives 0-100
+	lua_pushnil(L);
+	
+	return 3;
+	
 }
 
 int initLoveSystem(lua_State *L) {
 
 	luaL_Reg reg[] = {
-		{ "getOS",			systemGetOS			},
-		{ "getPowerInfo",	systemGetPowerInfo	},
+		{ "getOS",		   systemGetOS		 },
+		{ "getPowerInfo",  systemGetPowerInfo},
+		{ "getModel",	   systemGetModel    },
+		{ "getLanguage",   systemGetLanguage },
+		{ "getRegion",     systemGetRegion   },
 		{ 0, 0 },
 	};
 
