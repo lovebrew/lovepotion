2a5f4464d7fd45e9dfa0ddedbc16c0a443887c0c
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2016-03-20 06:38:23 +0000

    Table argument for setColor (Thanks TurtleP)

--- source/modules/graphics.c
+++ source/modules/graphics.c
@@ -78,9 +78,27 @@ int translateCoords(int *x, int *y) {
 
 static int graphicsSetBackgroundColor(lua_State *L) { // love.graphics.setBackgroundColor()
 
-	int r = luaL_checkinteger(L, 1);
-	int g = luaL_checkinteger(L, 2);
-	int b = luaL_checkinteger(L, 3);
+	int r, g, b;
+
+	if (lua_isnumber(L, -1)) {
+
+		r = luaL_checkinteger(L, 1);
+		g = luaL_checkinteger(L, 2);
+		b = luaL_checkinteger(L, 3);
+
+	} else if (lua_istable(L, -1)) {
+
+		for (int i = 1; i <= 4; i++) {
+
+			lua_rawgeti(L, 1, i);
+
+		}
+
+		r = luaL_checkinteger(L, -4);
+		g = luaL_checkinteger(L, -3);
+		b = luaL_checkinteger(L, -2);
+
+	}
 
 	sf2d_set_clear_color(RGBA8(r, g, b, 0xFF));
 
@@ -95,11 +113,20 @@ static int graphicsSetColor(lua_State *L) { // love.graphics.setColor()
 		currentR = luaL_checkinteger(L, 1);
 		currentG = luaL_checkinteger(L, 2);
 		currentB = luaL_checkinteger(L, 3);
-		currentA = luaL_optnumber(L, 4, 255);
+		currentA = luaL_optnumber(L, 4, currentA);
 
 	} else if (lua_istable(L, -1)) {
 
-		luaError(L, "Table support for setColor is not implemented yet. Use unpack(insertTableHere) until it is.");
+		for (int i = 1; i <= 4; i++) {
+
+			lua_rawgeti(L, 1, i);
+
+		}
+
+		currentR = luaL_checkinteger(L, -4);
+		currentG = luaL_checkinteger(L, -3);
+		currentB = luaL_checkinteger(L, -2);
+		currentA = luaL_optnumber(L, -1, currentA);
 
 	}
 
