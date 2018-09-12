5257c143f20ec1e145d964bc27c32b9660aba8f8
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2018-09-11 21:24:32 -0400

    Update drawable.cpp

--- source/common/drawable.cpp
+++ source/common/drawable.cpp
@@ -2,13 +2,7 @@
 #include "common/drawable.h"
 #include "modules/window.h"
 
-#define NO_APLHA (PNG_COLOR_TYPE_RGB | PNG_COLOR_TYPE_GRAY | PNG_COLOR_TYPE_PALETTE)
-#define GRAYSCALE (PNG_COLOR_TYPE_GRAY | PNG_COLOR_TYPE_GRAY_ALPHA)
-
-Drawable::Drawable(char * type) : Object(type)
-{
-
-}
+Drawable::Drawable(char * type) : Object(type) {}
 
 u32 * Drawable::LoadPNG(const char * path, char * buffer, size_t memorySize)
 {
@@ -59,24 +53,27 @@ u32 * Drawable::LoadPNG(const char * path, char * buffer, size_t memorySize)
     // Read any color_type into 8bit depth, ABGR format.
     // See http://www.libpng.org/pub/png/libpng-manual.txt
 
-    if (bit_depth == 16)
+    if(bit_depth == 16)
         png_set_strip_16(png);
 
-    if (color_type == PNG_COLOR_TYPE_PALETTE)
+    if(color_type == PNG_COLOR_TYPE_PALETTE)
         png_set_palette_to_rgb(png);
 
     // PNG_COLOR_TYPE_GRAY_ALPHA is always 8 or 16bit depth.
-    if (color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
+    if(color_type == PNG_COLOR_TYPE_GRAY && bit_depth < 8)
         png_set_expand_gray_1_2_4_to_8(png);
 
-    if (png_get_valid(png, info, PNG_INFO_tRNS) != 0)
+    if(png_get_valid(png, info, PNG_INFO_tRNS) != 0)
         png_set_tRNS_to_alpha(png);
 
     // These color_type don't have an alpha channel then fill it with 0xff.
-    if (color_type & NO_APLHA)
+    if(color_type == PNG_COLOR_TYPE_RGB ||
+        color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_PALETTE)
         png_set_filler(png, 0xFF, PNG_FILLER_AFTER);
 
-    if (color_type & GRAYSCALE)
+    if(color_type == PNG_COLOR_TYPE_GRAY ||
+        color_type == PNG_COLOR_TYPE_GRAY_ALPHA)
         png_set_gray_to_rgb(png);
 
     //output ABGR
