239e36941b46dd2faa6ca580081d7f2012896b9b
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2020-04-08 15:44:10 -0400

    switch: fix svcGetThreadPriority call for latest libnx

--- platform/switch/source/audio.cpp
+++ platform/switch/source/audio.cpp
@@ -5,11 +5,9 @@
 
 using namespace love;
 
-std::atomic<bool> THREAD_RUN;
-
 void Audio::_Initialize()
 {
-    u32 priority = 0;
+    s32 priority = 0;
     svcGetThreadPriority(&priority, CUR_THREAD_HANDLE);
     Result ret = threadCreate(&this->poolThread, AudioThreadRunner, (void *)&this->pool, NULL, 0x10000, priority - 1, 0);
 
