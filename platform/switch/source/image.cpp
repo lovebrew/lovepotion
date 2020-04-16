6e7f270bd151e9336a0b6d5a670cc347213abc59
Jeremy S. Postelnek <jeremy.postelnek@gmail.com>
2020-04-16 14:52:54 -0400

    switch: scale, rotate images

--- platform/switch/source/image.cpp
+++ platform/switch/source/image.cpp
@@ -39,8 +39,49 @@ void Image::Draw(const DrawArgs & args, love::Quad * quad, const Color & color)
 
     Quad::Viewport v = quad->GetViewport();
 
-    SDL_Rect source = {v.x, v.y, v.w, v.h};
-    SDL_Rect destin = {args.x, args.y, v.w, v.h};
+    /* do some scaling magic */
 
-    SDL_RenderCopyEx(WINDOW_MODULE()->GetRenderer(), this->texture, &source, &destin, 0.0, nullptr, SDL_RendererFlip::SDL_FLIP_NONE);
+    double scaleFactorX = quad->GetTextureWidth() / this->width;
+    double scaleSizingX = this->width / quad->GetTextureWidth();
+
+    v.x *= scaleSizingX;
+    v.w *= scaleSizingX;
+
+    double scaleFactorY = quad->GetTextureHeight() / this->height;
+    double scaleSizingY = this->height / quad->GetTextureHeight();
+
+    v.y *= scaleSizingY;
+    v.h *= scaleSizingY;
+
+    double scaledWidth  = v.w * scaleFactorX * std::abs(args.scalarX);
+    double scaledHeight = v.h * scaleFactorY * std::abs(args.scalarY);
+
+    /* adjust position  and flip mode*/
+
+    float x = args.x;
+    float y = args.y;
+
+    if (args.scalarX < 0)
+        x -= scaledWidth;
+    else if (args.scalarY < 0)
+        y -= scaledHeight;
+
+    SDL_RendererFlip flipHorizonal = (args.scalarX < 0) ? SDL_FLIP_HORIZONTAL : SDL_FLIP_NONE;
+    SDL_RendererFlip flipVertical  = (args.scalarY < 0) ? SDL_FLIP_VERTICAL   : SDL_FLIP_NONE;
+
+    /* render */
+
+    SDL_Rect source = {v.x, v.y, v.w, v.h};
+    SDL_Rect destin =
+    {
+        (int)round(x - (args.offsetX * args.scalarX)),
+        (int)round(y - (args.offsetY * args.scalarY)),
+        (int)ceil(scaledWidth),
+        (int)ceil(scaledHeight)
+    };
+
+    SDL_Point center = {(int)(args.offsetX * args.scalarX), (int)(args.offsetY * args.scalarY)};
+    double rotation = (args.r * 180 / M_PI);
+
+    SDL_RenderCopyEx(WINDOW_MODULE()->GetRenderer(), this->texture, &source, &destin, rotation, &center, (SDL_RendererFlip)(flipVertical | flipHorizonal));
 }
