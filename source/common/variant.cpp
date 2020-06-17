f8132dee659b1f5c1b6227eeca0791b2d88065bf
TurtleP <jpostelnek@outlook.com>
2020-06-16 23:45:16 -0400

    add variants to handle tables

--- source/common/variant.cpp
+++ source/common/variant.cpp
@@ -29,6 +29,8 @@ Variant::~Variant()
     }
     else if (self == Type::STRING)
         this->GetValue<Type::STRING>()->Release();
+    else if (self == Type::TABLE)
+        this->GetValue<Type::TABLE>()->Release();
 }
 
 Variant::Variant(love::Type * loveType, Object * object)
@@ -55,10 +57,7 @@ Variant::Variant(const char * string, size_t length)
         this->variant = str;
     }
     else
-    {
-        SharedString * str = new SharedString(string, length);
-        this->variant = str;
-    }
+        this->variant = new SharedString(string, length);
 }
 
 Variant::Variant(const std::string & string) : Variant(string.c_str(), string.length())
@@ -78,6 +77,8 @@ Variant & Variant::operator=(const Variant & v)
     }
     else if (other == Type::STRING)
         v.GetValue<Type::STRING>()->Retain();
+    else if (other == Type::TABLE)
+        v.GetValue<Type::TABLE>()->Retain();
 
     Type self = this->GetType();
     Proxy selfProxy;
@@ -91,10 +92,17 @@ Variant & Variant::operator=(const Variant & v)
     }
     else if (self == Type::STRING)
         this->GetValue<Type::STRING>()->Release();
+    else if (self == Type::TABLE)
+        this->GetValue<Type::TABLE>()->Release();
 
     variant = v.variant;
 
     return *this;
+}
+
+Variant::Variant(std::vector<std::pair<Variant, Variant>> * table)
+{
+    this->variant = new SharedTable(table);
 }
 
 Variant::Variant(const Variant & other) : variant(other.variant)
@@ -103,6 +111,8 @@ Variant::Variant(const Variant & other) : variant(other.variant)
         this->GetValue<Type::STRING>()->Retain();
     else if (this->GetType() == Type::LOVE_OBJECT && this->GetValue<Type::LOVE_OBJECT>().object != nullptr)
         this->GetValue<Type::LOVE_OBJECT>().object->Retain();
+    else if (this->GetType() == Type::TABLE)
+        this->GetValue<Type::TABLE>()->Retain();
 }
 
 Variant::Variant(Variant && other) : variant(std::move(other.variant))
@@ -124,6 +134,8 @@ std::string Variant::GetTypeString() const
         return "object";
     else if (type == Type::LUSERDATA)
         return "light userdata";
+    else if (type == Type::TABLE)
+        return "table";
 
     return "unknown";
 }
@@ -135,7 +147,7 @@ Variant::Type Variant::GetType() const
     return retval;
 }
 
-Variant Variant::FromLua(lua_State * L, int n)
+Variant Variant::FromLua(lua_State * L, int n, std::set<const void *> * tableSet)
 {
     Proxy * proxy = nullptr;
     const char * string;
@@ -166,6 +178,55 @@ Variant Variant::FromLua(lua_State * L, int n)
                 Luax::TypeErrror(L, n, "love type");
                 return Variant(Nil());
             }
+        case LUA_TTABLE:
+        {
+            bool success = true;
+            std::set<const void *> topTableSet;
+
+            std::vector<std::pair<Variant, Variant>> * table = new std::vector<std::pair<Variant, Variant>>();
+
+            // We can use a pointer to a stack-allocated variable because it's
+			// never used after the stack-allocated variable is destroyed.
+            if (tableSet == nullptr)
+                tableSet = &topTableSet;
+
+            // Check it's not already serialized
+            const void * tablePointer = lua_topointer(L, n);
+
+            {
+                auto result = tableSet->insert(tablePointer);
+                if (!result.second) // Insertion failure
+                    throw love::Exception("Cycle detected in table.");
+            }
+
+            size_t length = lua_objlen(L, -1);
+            if (length > 0)
+                table->reserve(length);
+
+            lua_pushnil(L);
+
+            while (lua_next(L, n))
+            {
+                table->emplace_back(FromLua(L, -2, tableSet), FromLua(L, -1, tableSet));
+                lua_pop(L, 1);
+
+                const auto & p = table->back();
+
+                if (p.first.GetType() == Type::UNKNOWN || p.second.GetType() == Type::UNKNOWN)
+                {
+                    success = false;
+                    break;
+                }
+            }
+
+            // remove the table from the set
+            tableSet->erase(tablePointer);
+
+            if (success)
+                return Variant(table);
+            else
+                delete table;
+        }
         default:
             break;
     }
@@ -208,6 +269,25 @@ void Variant::ToLua(lua_State * L) const
             lightUserdata = GetValue<Type::LUSERDATA>();
             lua_pushlightuserdata(L, lightUserdata);
             break;
+        case Type::TABLE:
+        {
+            auto table = GetValue<Type::TABLE>()->table;
+            size_t size = table->size();
+
+            lua_createtable(L, 0, size);
+
+            for (size_t index = 0; index < size; ++index)
+            {
+                std::pair<Variant, Variant> & keyValue = (*table)[index];
+
+                keyValue.first.ToLua(L);
+                keyValue.second.ToLua(L);
+
+                lua_settable(L, -3);
+            }
+
+            break;
+        }
         case Type::NIL:
         default:
             lua_pushnil(L);
