49e7add01bb2c03d5facfc42180e6146698eec05
TurtleP <jpostelnek@outlook.com>
2021-05-26 12:15:58 -0400

    3ds font module works

--- platform/3ds/include/objects/glyphdata/glyphdata.h
+++ platform/3ds/include/objects/glyphdata/glyphdata.h
@@ -6,7 +6,11 @@ namespace love
 {
     class GlyphData : public common::GlyphData
     {
-        public:
-            GlyphData* Clone() const override;
+      public:
+        GlyphData(uint32_t glyph, GlyphMetrics metrics);
+
+        GlyphData(const GlyphData& other);
+
+        GlyphData* Clone() const override;
     };
 } // namespace love
--- platform/3ds/source/objects/glyphdata.cpp
+++ platform/3ds/source/objects/glyphdata.cpp
@@ -2,6 +2,12 @@
 
 using namespace love;
 
+GlyphData::GlyphData(uint32_t glyph, GlyphMetrics metrics) : common::GlyphData(glyph, metrics)
+{}
+
+GlyphData::GlyphData(const GlyphData& other) : common::GlyphData(other)
+{}
+
 GlyphData* GlyphData::Clone() const
 {
     return new GlyphData(*this);
