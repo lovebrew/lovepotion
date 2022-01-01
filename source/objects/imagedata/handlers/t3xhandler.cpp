8314b4bc13f1f4ede975c8dfcf7812190550d1ec
TurtleP <jpostelnek@outlook.com>
2021-12-31 21:19:04 -0500

    t3xhandler: some adjustments

--- source/objects/imagedata/handlers/t3xhandler.cpp
+++ source/objects/imagedata/handlers/t3xhandler.cpp
@@ -6,17 +6,11 @@
 #include "common/colors.h"
 
 #include "modules/data/datamodule.h"
-
-#include "debug/logger.h"
+#include <memory>
 
 using namespace love;
 
 #if defined(__3DS__)
-bool T3XHandler::CanEncode(PixelFormat rawFormat, EncodedFormat encodedFormat)
-{
-    return encodedFormat == ENCODED_T3X;
-}
-
 bool T3XHandler::CanDecode(Data* data)
 {
     Tex3DSHeader header {};
@@ -41,90 +35,20 @@ T3XHandler::DecodedImage T3XHandler::Decode(Data* data)
 
     DecodedImage decoded {};
 
-    decoded.width  = header.width;
-    decoded.height = header.height;
+    decoded.width  = 1 << (header.width_log2 + 3);
+    decoded.height = 1 << (header.height_log2 + 3);
     decoded.format = PIXELFORMAT_RGBA8;
     decoded.size   = 4 * header.width * header.height;
+    decoded.data   = new uint8_t[header.size];
 
-    decoded.data = new uint8_t[decoded.size];
-
-    uint8_t* uncompressed = new uint8_t[header.size];
-
-    bool wasDecompressed = decompress(uncompressed, header.size, NULL,
-                                      (uint8_t*)data->GetData() + sizeof(header), header.size);
-
-    if (!wasDecompressed)
-        memcpy(decoded.data, (uint8_t*)data->GetData() + sizeof(header), header.size);
-    else
-        memcpy(decoded.data, uncompressed, decoded.size);
-
-    delete[] uncompressed;
-
-    return decoded;
-}
-
-T3XHandler::EncodedImage T3XHandler::Encode(const DecodedImage& decoded,
-                                            EncodedFormat encodedFormat)
-{
-    if (!this->CanEncode(decoded.format, encodedFormat))
-        throw love::Exception("t3x encoder cannot encode to non-t3x format.");
-
-    Tex3DSHeader header {};
-
-    header.numSubTextures = 1;
-
-    // power-of-two size
-    header.width_log2  = log2(NextPO2(decoded.width + 2)) - 3;
-    header.height_log2 = log2(NextPO2(decoded.height + 2)) - 3;
-
-    header.type         = 0; //< GPU_TEX_2D
-    header.format       = 0; //< GPU_RGBA8
-    header.mipmapLevels = 0;
-
-    // source texture size
-    header.width  = decoded.width;
-    header.height = decoded.height;
-
-    unsigned border = 1;
-
-    unsigned powTwoWidth  = NextPO2(decoded.width + 2);
-    unsigned powTwoHeight = NextPO2(decoded.height + 2);
-
-    header.left   = (border / (float)powTwoWidth) * 1024.0f;
-    header.top    = (border / (float)powTwoHeight) * 1024.0f;
-    header.right  = ((decoded.width + border) / (float)powTwoWidth) * 1024.0f;
-    header.bottom = ((decoded.height + border) / (float)powTwoHeight) * 1024.0f;
-
-    header.size = 4 * powTwoWidth * powTwoHeight;
-
-    header.compressionType = 0;
-
-    EncodedImage encoded {};
-
-    size_t size = sizeof(header) + header.size;
-
-    encoded.data = new uint8_t[size];
-    encoded.size = size;
-
-    memcpy(encoded.data, &header, sizeof(header));
-
-    uint8_t* output = encoded.data + sizeof(header);
-    memset(output, 0, header.size);
-
-    for (unsigned y = 1; y <= (size_t)decoded.height; ++y)
+    if (!decompress(decoded.data, header.size, NULL,
+                    reinterpret_cast<uint8_t*>(data->GetData()) + sizeof(Tex3DSHeader) - 4,
+                    data->GetSize() - sizeof(Tex3DSHeader) - 4))
     {
-        for (unsigned x = 1; x <= (size_t)decoded.width; ++x)
-        {
-            const unsigned source_off = (y - 1) * decoded.width + (x - 1);
-            const unsigned dest_off   = ((((y >> 3) * (powTwoWidth >> 3) + (x >> 3)) << 6) +
-                                       ((x & 1) | ((y & 1) << 1) | ((x & 2) << 1) | ((y & 2) << 2) |
-                                        ((x & 4) << 2) | ((y & 4) << 3)));
-
-            memcpy((u32*)output + dest_off, (u32*)decoded.data + source_off, sizeof(u32));
-        }
+        throw love::Exception("Failed to decompress t3x data.");
     }
 
-    return encoded;
+    return decoded;
 }
 #endif
 
