83022cb7f362c872789917e63f255f9b82f63e41
TurtleP <jpostelnek@outlook.com>
2022-07-21 17:22:21 -0400

    mark d'tor virtual

--- include/objects/imagedata/imagedatabase.h
+++ include/objects/imagedata/imagedatabase.h
@@ -8,7 +8,7 @@ namespace love
     class ImageDataBase : public Data
     {
       public:
-        ~ImageDataBase() {};
+        virtual ~ImageDataBase() {};
 
         PixelFormat GetFormat() const;
 
