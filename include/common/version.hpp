720fa068365d5e0a796c254e0466ddc8f64ed00f
TurtleP <jpostelnek@outlook.com>
2026-01-04 20:01:54 -0500

    version: cleanup

--- include/common/version.hpp
+++ include/common/version.hpp
@@ -2,77 +2,50 @@
 
 #include <array>
 #include <compare>
-#include <string>
-#include <string_view>
 
 namespace love
 {
     struct Version
     {
-      public:
-        constexpr Version() : major(0), minor(0), revision(0)
+        uint8_t major = 0;
+        uint8_t minor = 0;
+        uint8_t patch = 0;
+
+        constexpr Version() = default;
+
+        constexpr Version(uint8_t major, uint8_t minor, uint8_t patch) :
+            major(major),
+            minor(minor),
+            patch(patch)
         {}
 
-        constexpr Version(std::string_view v) : major(0), minor(0), revision(0)
+        constexpr Version(const char* v)
         {
-            auto nposzero = [](size_t x) { return x == std::string_view::npos ? 0 : x; };
-
-            auto major = v.substr(0, nposzero(v.find(DELIMITER)));
-
-            size_t minorIdx = std::min(major.size() + 1, v.size());
-            auto minor      = v.substr(minorIdx, nposzero(v.find(DELIMITER, minorIdx)));
-
-            size_t microIdx = std::min(major.size() + 1 + minor.size() + 1, v.size());
-            auto micro      = v.substr(microIdx, nposzero(v.find_first_not_of(DIGITS, microIdx)));
-
-            if (major.size() != 0)
-                this->major = atoi(major);
-
-            if (minor.size() != 0)
-                this->minor = atoi(minor);
-
-            if (micro.size() != 0)
-                this->revision = atoi(micro);
-
-            if (major.size() == 0 && minor.size() == 0 && micro.size() == 0)
-                this->major = atoi(v);
+            // clang-format off
+            major = parse_number(v);
+            if (*v == '.') ++v;
+            minor = parse_number(v);
+            if (*v == '.') ++v;
+            patch = parse_number(v);
+            // clang-format on
         }
-
-        template<typename T>
-        requires std::constructible_from<std::string_view, T> || std::convertible_to<T, std::string_view>
-        constexpr Version(T&& version) : Version(std::string_view(std::forward<T>(version)))
-        {}
-
-        Version(uint8_t major, uint8_t minor, uint8_t micro) : major(major), minor(minor), revision(micro)
-        {}
 
         constexpr std::strong_ordering operator<=>(const Version&) const noexcept = default;
 
-        uint8_t major;
-        uint8_t minor;
-        uint8_t revision;
-
       private:
-        static constexpr char DELIMITER     = '.';
-        static constexpr const char* DIGITS = "123457890";
-
-        constexpr static uint8_t atoi(std::string_view view)
+        static constexpr uint8_t parse_number(const char*& s)
         {
             uint8_t value = 0;
-
-            for (const auto& character : view)
-            {
-                if ('0' <= character && character <= '9')
-                    value = value * 10 + character - '0';
-            }
+            while (*s >= '0' && *s <= '9')
+                value = value * 10 + (*s++ - '0');
 
             return value;
         }
     };
 
-    static constexpr Version LOVE_POTION(__APP_VERSION__);
-    static constexpr Version LOVE_FRAMEWORK(__LOVE_VERSION__);
+    static constexpr Version POTION_VERSION(LOVE_POTION_VERSION_STRING);
+    static constexpr Version VERSION(LOVE_VERSION_STRING);
 
-    static constexpr const char* CODENAME                     = "Bestest Friend";
-    static constexpr std::array<const char*, 1> COMPATIBILITY = { __LOVE_VERSION__ };
+    static constexpr const char* VERSION_CODENAME                     = "Bestest Friend";
+    static constexpr std::array<const char*, 1> VERSION_COMPATIBILITY = { LOVE_VERSION_STRING };
 } // namespace love
